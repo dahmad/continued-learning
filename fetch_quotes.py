be453ae1858f367c59726b9e84b39a65f69d0697
dahmad <daniyal.aziz.ahmad@gmail.com>
2022-01-29 10:07:51 -0800

    Added try/except to fetch_quotes to ignore formatting failures

--- fetch_quotes.py
+++ fetch_quotes.py
@@ -15,17 +15,20 @@ quotes = []
 for file_path_target in file_paths_target:
     title = file_path_target.split('/')[-1].split('.')[0]
     with open(file_path_target, 'r') as stream:
-        data = load(stream, Loader=Loader)
-        chapters = data.keys()
-        for chapter in chapters:
-            quotes_in_chapter = data[chapter]['quotes']
-            for quote in quotes_in_chapter:
-                if quote != "":
-                    quotes.append({
-                        "text": quote,
-                        "title": capitalize_words(title),
-                        "chapter": chapter
-                    })
+        try:
+            data = load(stream, Loader=Loader)
+            chapters = data.keys()
+            for chapter in chapters:
+                quotes_in_chapter = data[chapter]['quotes']
+                for quote in quotes_in_chapter:
+                    if quote != "":
+                        quotes.append({
+                            "text": quote,
+                            "title": capitalize_words(title),
+                            "chapter": chapter
+                        })
+        except:
+            pass
 
 for i, quote in enumerate(quotes):
     attribution = f"\n- {quote['title']}, {quote['chapter']}"
